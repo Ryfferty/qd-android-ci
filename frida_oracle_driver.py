4e676fe007066cbae9a5abf4053c517a021610a5
ryz <1470769489@qq.com>
2026-09-18 20:18:59 +0800

    passive v2: fixed deeplink Intent + reader page trigger

--- frida_oracle_driver.py
+++ frida_oracle_driver.py
@@ -10,7 +10,7 @@ def main():
     
     pid = int(sys.argv[1])
     
-    for p in ["hook_fock_passive.js", "ci/hook_fock_passive.js", "hook_fock_oracle_v4.js", "ci/hook_fock_oracle_v4.js"]:
+    for p in ["hook_fock_passive_v2.js", "ci/hook_fock_passive_v2.js", "hook_fock_passive.js", "ci/hook_fock_passive.js"]:
         if os.path.exists(p):
             with open(p) as f:
                 js = f.read()
