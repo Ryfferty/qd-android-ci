cd562beb53260a24d22c12cf5ec4862dea253788
ryz <1470769489@qq.com>
2026-09-18 16:43:26 +0800

    oracle v4: wait for app load + try addKeys orderings + addKeypool

--- frida_oracle_driver.py
+++ frida_oracle_driver.py
@@ -10,7 +10,7 @@ def main():
     
     pid = int(sys.argv[1])
     
-    for p in ["hook_fock_oracle_v3.js", "ci/hook_fock_oracle_v3.js", "hook_fock_oracle_v2.js", "ci/hook_fock_oracle_v2.js", "hook_fock_oracle.js", "ci/hook_fock_oracle.js"]:
+    for p in ["hook_fock_oracle_v4.js", "ci/hook_fock_oracle_v4.js", "hook_fock_oracle_v3.js", "ci/hook_fock_oracle_v3.js"]:
         if os.path.exists(p):
             with open(p) as f:
                 js = f.read()
